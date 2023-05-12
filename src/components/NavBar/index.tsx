13d7c729dac15ef9a75e485b76e319fbd1c06c5d
Buletiss <vinicius1997feitosa@gmail.com>
2023-05-12 00:18:53 -0300

    chore: :construction: navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,15 +1,40 @@
-import { Flex, ListItem, OrderedList } from '@chakra-ui/react';
+import {
+  Image,
+  Button,
+  Flex,
+  Link,
+  Box,
+  ListItem,
+  OrderedList,
+} from '@chakra-ui/react';
 
 export const NavBar = () => {
   return (
     <>
-      <OrderedList display="flex" alignContent="center" justifyContent="center">
-        <ListItem>a</ListItem>
-        <ListItem>a</ListItem>
-        <ListItem>a</ListItem>
-        <ListItem>a</ListItem>
-        <ListItem>a</ListItem>
-      </OrderedList>
+      <Flex
+        alignItems="center"
+        justifyContent="space-between"
+        background="0,0,0,0,0"
+      >
+        <Image src="/testIcon.png" alt="iconImage" w={100} h={100} />
+        <Flex alignItems="center">
+          <OrderedList display="flex" paddingRight={5} color="white">
+            <Link href="#" paddingLeft="30px">
+              <ListItem>a</ListItem>
+            </Link>
+            <Link href="#" paddingLeft="30px">
+              <ListItem>b</ListItem>
+            </Link>
+            <Link href="#" paddingLeft="30px">
+              <ListItem>c</ListItem>
+            </Link>
+            <Link href="#" paddingLeft="30px">
+              <ListItem>d</ListItem>
+            </Link>
+          </OrderedList>
+          <Button>test</Button>
+        </Flex>
+      </Flex>
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { Loading } from '../components/Loading';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Box } from '@chakra-ui/react';
 import { NavBar } from '../components/NavBar';
 import { useLoading } from '../hooks/use-loading';
 
 export default function Home() {
   const { showLoading } = useLoading();
-  return <>{showLoading ? <Loading /> : <NavBar />}</>;
+  return (
+    <>
+      <Box bg="#191D32" h="100vh">
+        {showLoading ? <Loading /> : <NavBar />}
+      </Box>
+    </>
+  );
 }
