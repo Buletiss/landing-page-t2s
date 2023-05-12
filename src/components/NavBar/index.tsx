e028a7b28eb9912956c78693a943107d91597d15
Buletiss <vinicius1997feitosa@gmail.com>
2023-05-11 22:46:20 -0300

    feat: :sparkles: home

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,15 @@
+import { Flex, ListItem, OrderedList } from '@chakra-ui/react';
+
+export const NavBar = () => {
+  return (
+    <>
+      <OrderedList display="flex" alignContent="center" justifyContent="center">
+        <ListItem>a</ListItem>
+        <ListItem>a</ListItem>
+        <ListItem>a</ListItem>
+        <ListItem>a</ListItem>
+        <ListItem>a</ListItem>
+      </OrderedList>
+    </>
+  );
+};
