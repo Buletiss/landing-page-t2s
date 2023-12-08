47e2b9b32a1f551a47dfcd71f7b914944c3a62ef
Buletiss <vinicius1997feitosa@gmail.com>
2023-12-08 06:16:16 -0300

    cat on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
-import { Loading } from '../components/Loading';
-import { Flex, Box } from '@chakra-ui/react';
-import { NavBar } from '../components/NavBar';
-import { useLoading } from '../hooks/use-loading';
+import { Loading } from "../components/Loading";
+import { Flex, Box, Img } from "@chakra-ui/react";
+import { NavBar } from "../components/NavBar";
+import { useLoading } from "../hooks/use-loading";
+import cat from "../image/cat.gif";
 
 export default function Home() {
   const { showLoading } = useLoading();
   return (
     <>
       <Box bg="#191D32" h="100vh">
-        {showLoading ? <Loading /> : <NavBar />}
+        {/* {showLoading ? <Loading /> : <NavBar />} */}
+        <Img src="/cat.gif"></Img>
       </Box>
     </>
   );
