87c1fd85685b724efa541cc92aa85baeb8f537b1
Buletiss <vinicius1997feitosa@gmail.com>
2023-12-11 17:44:27 -0300

    style: :art: ajuste na estrutura base

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Flex, ChakraProvider } from '@chakra-ui/react';
-import Home from './index';
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import Home from "./index";
 
 export default function App() {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,19 @@
 import { Loading } from "../components/Loading";
-import { Flex, Box, Img } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import { NavBar } from "../components/NavBar";
 import { useLoading } from "../hooks/use-loading";
-import cat from "../image/cat.gif";
 
 export default function Home() {
   const { showLoading } = useLoading();
   return (
     <>
-      <Box bg="#191D32" h="100vh">
-        {/* {showLoading ? <Loading /> : <NavBar />} */}
-        <Img src="/cat.gif"></Img>
+      <Box bg="white" h="100vh">
+        {showLoading ? <Loading /> : <NavBar />}
+        <Flex alignItems="center" justifyContent="center">
+          <Heading as="h1" size="4xl">
+            Under Construction
+          </Heading>
+        </Flex>
       </Box>
     </>
   );
